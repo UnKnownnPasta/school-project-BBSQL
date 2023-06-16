cfb5c33c11075911ead0e948fe8cde67cc39c503
UnKnownn <pandaepic01@gmail.com>
2023-06-16 18:29:57 +0530

    Bug Fixes + SQL + Login fixed

--- iter-2.4.py
+++ iter-2.4.py
@@ -1,30 +1,49 @@
 from tkinter import messagebox, font, ttk
 from tkinter import *
 import mysql.connector as sql
-import time, re
-from PIL import Image, ImageTk
-
-global con, cur
-con = sql.connect(host='localhost', user='root', password='root')
-cur = con.cursor()
-cur.execute('use bloodbank')
+from re import findall
+from time import sleep
 
 
-global root, bg_img, logo_img, arrow
-root = Tk()
-bg_img_1 = PhotoImage(file='bg-blur-v2.png')
-bg_img_2 = PhotoImage(file='bg-unblur.png')
-logo_img = PhotoImage(file='l3-2.png')
-logo_img2 = PhotoImage(file='l2-2.png')
-arrow = PhotoImage(file='skills-3.png')
-root.title('Blood Bank Mng')
-root.iconphoto(False, PhotoImage(file='logo.png'))
-root.geometry(f"{940}x{500}+{(root.winfo_screenwidth() - 940) // 2}+{(root.winfo_screenheight() - 500) // 2}")
-root.resizable(False, False)
+def init():
+    global bg_img_1, bg_img_2, logo_img, logo_img2, arrow, root
+    # root.geometry(f"{300}x{300}+{(root.winfo_screenwidth() - 300) // 2}+{(root.winfo_screenheight() - 300) // 2}")
+    root = Tk()
+    bg_img_1 = PhotoImage(file='bg-blur-v2.png')
+    bg_img_2 = PhotoImage(file='bg-unblur.png')
+    logo_img = PhotoImage(file='l3-2.png')
+    logo_img2 = PhotoImage(file='l2-2.png')
+    arrow = PhotoImage(file='skills-3.png')
+
+    global con, cur
+    try:
+        con = sql.connect(host='localhost', user='root', password='root')
+        cur = con.cursor()
+    except:
+        messagebox.showerror('Error', 'Failed to connect to SQL')
+    cur.execute('show databases;')
+    res = any(db[0] == 'bloodbank' for db in cur.fetchall())
+    if res == False:
+        cur.execute('create database bloodbank')
+    cur.execute('use bloodbank')
+
+    try:
+        cur.execute('create table if not exists Hospital (HospitalID int(4) auto_increment primary key, HospitalName varchar(100) unique, Password varchar(20), Contact varchar(100), PinCode char(6) not null)')   #.zfill
+        cur.execute('create table if not exists BloodBank (BloodType char(2), Units int not null, RhFactor char(8))')
+        cur.execute('create table if not exists Donor (Name varchar(40), Age int(3), Gender char(20), BloodGroup char(2), HospitalID int(4), foreign key (HospitalID) references Hospital(HospitalID))')
+        cur.execute('create table if not exists Recipient (Name varchar(40), Age int(3), DateOfTransfer date, HospitalID int(4), BloodType char(2), foreign key (HospitalID) references Hospital(HospitalID))')
+    except:
+        messagebox.showerror('Error', 'Something went wrong while initializing tables.')
+
+    root.title('Blood Bank Mng')
+    root.iconphoto(False, PhotoImage(file='logo.png'))
+    root.resizable(False, False)
+    root.geometry(f"{940}x{500}+{(root.winfo_screenwidth() - 940) // 2}+{(root.winfo_screenheight() - 500) // 2}")
+    Login()
 
 
 def SignUp():
-    global hospName, pinCode, Contact, submBtnSu, canvasSu, swchBtnSu
+    global hospName, pinCode, Contact, submBtnSu, canvasSu, swchBtnSu, PassWord
     canvasSu = Canvas(root, width='940', height='500', highlightthickness=0)
     canvasSu.create_image(0, 0, image=bg_img_1, anchor='nw')
     canvasSu.pack(side = "top", fill = "both", expand = True)
@@ -99,7 +118,7 @@ def Login():
     userPass.bind('<FocusIn>', lambda event: setText(userPass, 'Password'))
     userPass.bind('<FocusOut>', lambda event: restoreText(userPass, 'Password'))
 
-    submBtnLi = Button(root, text='Login', background='#6CB4EE', relief=FLAT, padx=20, pady=10, activebackground='#6CB4EE', command= lambda: program(userName.get(), userPass.get()))
+    submBtnLi = Button(root, text='Login', background='#6CB4EE', relief=FLAT, padx=20, pady=10, activebackground='#6CB4EE', command= lambda: loginSubm(userName.get(), userPass.get()))
     signBtnLi = Button(root, text='Sign Up', background='#6CB4EE', relief=FLAT, padx=20, pady=10, activebackground='#6CB4EE', command=switchL_S)
 
     userName.place(x=240, y=260)
@@ -109,7 +128,7 @@ def Login():
 
 
 def switchS_L(): # Signup to Login
-    w = [hospName, pinCode, Contact, submBtnSu, canvasSu, swchBtnSu]
+    w = [hospName, pinCode, Contact, submBtnSu, canvasSu, swchBtnSu, PassWord]
     for i in w:
         i.destroy()
     Login()
@@ -120,20 +139,47 @@ def switchL_S(): # Login to Signup
     SignUp()
 
 
-def SignSubm(pstCde, cntct, hospNme, paswrd):
-    verf = pinVerify(pstCde)
+def SignSubm(postCode, contact, hospName, passwrd):
+    verf = pinVerify(postCode)
     if verf == False:
         messagebox.showerror('Failed', 'Invalid Pin Code.')
         return
-    if hospNme == 'Hospital Name' or len(hospNme) == 0:
+    if hospName == 'Hospital Name' or len(hospName) == 0:
         messagebox.showerror('Failed', 'Invalid Hospital Name.')
         return
-    if len(cntct) == 0 or cntct == 'Contact (Phone No./email)':
+    if len(contact) == 0 or contact == 'Contact (Phone No./email)':
         messagebox.showerror('Failed', 'No Valid Contacts provided.')
         return
         
+    try:
+        vals = (hospName, passwrd, contact, postCode)
+        query = "insert into Hospital (HospitalName, Password, Contact, PinCode) values (%s, %s, %s, %s)"
+        cur.execute(query, vals)
+        con.commit()
+    except:
+        messagebox.showerror('Error', 'Failed to signup. Try again.')
+        return
+
     messagebox.showinfo('Success', 'Account created successfully! Login to it here')
     switchS_L()
+
+
+def loginSubm(un, pw):
+    for i in [un, pw]:
+        if i == 'User Name' or i == 'Password' or len(i) == '' or i == ' ':
+            messagebox.showerror('Error', 'Invalid login details')
+            return
+    query = "select * from hospital where HospitalName=%s and Password=%s"
+    values = (un, pw)
+    cur.execute(query, values)
+    res = cur.fetchone()
+    if res == None:
+        messagebox.showerror('Error', 'Login details are not correct.')
+        return
+    ww = [canvasLi, userName, userPass, submBtnLi, signBtnLi]
+    for x in ww:
+        x.destroy()
+    program(un, pw)
 
 
 def pinVerify(pin) -> True:
@@ -146,17 +192,13 @@ def pinVerify(pin) -> True:
     status = False
     for i in range(8):
         regx = reg.readline().strip().replace('/', '')
-        m = re.findall(regx, pin)
+        m = findall(regx, pin)
         if len(m) != 0:
             status = True
     return status
 
 
-def program(un, pw):
-    w_super = [canvasLi, userName, userPass, submBtnLi, signBtnLi]
-    for i in w_super:
-        i.destroy()
-    
+def program(u, p):
     title_bar = Frame(root, bg="#D22B2B", height=30)
     title_bar.pack(fill=X)
     title_bar.place(rely=0.05, relwidth=1) # relwidth gives the fill to be 100%, rely keeps it 500*0.05 = 25 pixels from top
@@ -167,30 +209,32 @@ def program(un, pw):
     title_label = Label(root, text="Blood Bank Management", fg="white", bg="#D22B2B", font=('Josefin Sans', 17), pady=0)
     title_label.place(x=50, y=25)
 
-    global stg
-    stg = ["", 0]
+    global storage
+    storage = ["", 0]
     def scroll_text(txt):
-        global stg
-        if stg[0] != txt:
-            stg[0] += txt
-            label.configure(text=" " + stg[0])
+        global storage
+        storage[0] += txt
+        scrollbar.configure(text=storage[0])
 
-        def rotate():
-            text = label.cget("text")
-            rotated_text = text[1:] + text[0]
-            if rotated_text == text:
-                rotated_text = text
-            label.config(text=" " + rotated_text + "  ")
-            label.after(300, rotate)
+        def rotate(): # Text Capacity = 187 + word length
+            text = scrollbar.cget("text")
+            if len(text) >= 187+len(storage[0]):
+                storage[0] = ""
+            scrollbar.config(text=text + "  ")
+            scrollbar.after(100, rotate)
 
-        if not stg[1]:
-            stg[1] = 1; rotate()
+        if not storage[1]:
+            storage[1] = 1; rotate()
 
-    label = Label(root, font=("Arial", 12), anchor=NE, bg="black", bd=2, fg="white", width=104, highlightcolor='blue', highlightthickness=1)
-    label.place(x=-1, y=0)
-    scroll_text('   This is some text   ')
+
+    scrollbar = Label(root, font=("Arial", 12), anchor=NE, bg="black", fg="white", width=104)
+    scrollbar.place(x=0, y=1)
+    scroll_text(f'   Welcome {u}!   ')
+    
     a = Button(root, text= 'a', command= lambda: scroll_text('   nyooooooooom   '))
     a.place(x=30, y=300)
     
-SignUp()
-root.mainloop()
+if __name__ == '__main__':
+    # Start program
+    init()
+    root.mainloop()
